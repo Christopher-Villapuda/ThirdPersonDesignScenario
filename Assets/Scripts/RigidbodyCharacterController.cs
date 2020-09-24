6c64472ed59f5fccc4c9cf60689d34f06b98c50c
Christopher-Villapuda <49170925+Christopher-Villapuda@users.noreply.github.com>
2020-09-24 14:49:00 -0500

    MaxSpeed Change
    Added in a max speed cap to the player.

--- Assets/Scripts/RigidbodyCharacterController.cs
+++ Assets/Scripts/RigidbodyCharacterController.cs
@@ -7,6 +7,9 @@ public class RigidbodyCharacterController : MonoBehaviour
     [SerializeField]
     private float accelerationForce = 10;
 
+    [SerializeField]
+   private float maxSpeed =2 ;
+
     private new Rigidbody rigidbody;
     private Vector2 input;
 
@@ -18,7 +21,12 @@ public class RigidbodyCharacterController : MonoBehaviour
     private void FixedUpdate()
     {
         var inputDirection = new Vector3(input.x, 0, input.y);
-        rigidbody.AddForce(inputDirection*accelerationForce);
+        //rigidbody.AddForce(inputDirection*accelerationForce);
+        if (rigidbody.velocity.magnitude < maxSpeed)
+        {
+            rigidbody.AddForce(inputDirection * accelerationForce);
+        }
+        //rigidbody.velocity = Vector3.ClampMagnitude(rigidbody.velocity, maxSpeed);
     }
     private void Update()
     {
